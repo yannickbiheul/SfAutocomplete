a30c94d688079cd8eb852ced05dc6380c5fa8a28
BIHEUL Yannick <yannick.biheul@uhc-sante.fr>
2022-12-14 09:04:30 +0100

    balek

--- src/Controller/SimpleController.php
+++ src/Controller/SimpleController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/simple')]
+#[Route('/')]
 class SimpleController extends AbstractController
 {
     #[Route('/', name: 'app_simple_index', methods: ['GET'])]
